ef1469c514624846dbe6d27b4f1d5f8d07efab92
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-09-04 09:46:11 +0100

    learning MVC

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
-// Global app controller
-import num from './test';
-const x=23;
-console.log(`I imported ${num} from another module. Variable x is ${x}.`);
+import string from './models/Search';
+import { add as a, multiply, ID } from './views/searchView';
+import * as searchView from './views/searchView';
+console.log(`Using imported functions: add as a - ${a(ID, 2)} and multiply - ${multiply(3,5)}. As well as, ${string}`);
+console.log(`Using imported functions, importing everything: add - ${searchView.add(ID, 2)} and multiply - ${searchView.multiply(3,5)}.`);
