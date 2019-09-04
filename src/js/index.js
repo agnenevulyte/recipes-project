089b45c9616670113409acf61164758ab14a0c07
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-09-04 11:33:36 +0100

    built Search Controller.Adding state&eventListener

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,34 @@
 import Search from './models/Search';
-const search = new Search('pizza');
-console.log(search);
-console.log(search.getResults());
+
+
+/*
+Global state of the pp
+- Search object
+- Current recipe object
+- Shopping list object
+- Liked recipes
+*/
+const state = {};
+
+const controlSearch = async () => {
+    // get the query from view
+    const query ='pizza' // TODO
+
+    if (query) {
+        // new search object and add it to state
+        state.search = new Search(query);
+
+        // prepare UI for results
+
+        // search for recipes
+        await state.search.getResults();
+
+        // render rsults on UI
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
