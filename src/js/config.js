4fcbac4153fa9f712edb1e0538f3849c6c4514f2
Agne Nevulyte <agne.nevulyte@finsaeurope.com>
2019-09-11 14:07:32 +0100

    changed the key

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,3 @@
 export const proxy = "https://cors-anywhere.herokuapp.com/";
-export const key = "da13a79241ea5c34ea42f9e0352a1d22";
+export const key = "8bb70677911b3d35a246a41c03ace7fb";
+// key from https://www.food2fork.com/
